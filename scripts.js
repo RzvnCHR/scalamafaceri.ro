1073226e67d7b7c4c5b9a08278f8bd700aca2216
RzvnCHR <151965041+RzvnCHR@users.noreply.github.com>
2026-03-06 23:07:20 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+        var typed = new Typed(".auto-type", {
+      strings: ["^500", "Branding Design^1000", "Google Ads^1000","Facebook Ads^1000", "Web Design^1000",  "Social Media Management^1000"],
+      typeSpeed: 50,
+      backSpeed: 30,
+      loop: true
+    })
